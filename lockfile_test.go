77ccc0a6e96f3d3b22c8c0ef564fe6cc838b8762
Joseph Bernstein <joe@bountyapp.co>
2014-11-19 14:28:07 -0800

    add a couple basic tests

--- lockfile_test.go
+++ lockfile_test.go
@@ -1,12 +1,15 @@
-package lockfile_test
+package lockfile
 
 import (
-	lockfile "."
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
 )
 
 func ExampleLockfile() {
-	lock, err := lockfile.New("/tmp/lock.me.now.lck")
+	lock, err := New("/tmp/lock.me.now.lck")
 	if err != nil {
 		fmt.Println("Cannot init lock. reason: %v", err)
 		panic(err)
@@ -23,4 +26,87 @@ func ExampleLockfile() {
 
 	fmt.Println("Do stuff under lock")
 	// Output: Do stuff under lock
+}
+
+func SimpleLockTest(t *testing.T) {
+
+	temp := os.TempDir()
+	file := filepath.Join(temp, "lock.me.test")
+	lock, err := New(file)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = lock.TryLock()
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		err = lock.Unlock()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+}
+
+func TestBadLock(t *testing.T) {
+
+	temp := os.TempDir()
+	file := filepath.Join(temp, "lock.me.test")
+	if err := ioutil.WriteFile(file, []byte("asdf"), os.ModePerm); err != nil {
+		t.Error(err)
+	}
+
+	lock, err := New(file)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = lock.TryLock()
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		err = lock.Unlock()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+}
+
+func TestConcurrentAccess(t *testing.T) {
+
+	temp := os.TempDir()
+	file := filepath.Join(temp, "lock.me.test")
+	lock, err := New(file)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = lock.TryLock()
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		err = lock.Unlock()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+
+	lock2, err := New(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = lock2.TryLock()
+	if err == nil {
+		t.Error("expected lock to fail")
+	}
 }
