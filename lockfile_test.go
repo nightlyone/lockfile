ca08e186ba35f3c8c5549d126250714d8e57cfb3
Stewart Borle <stewi1014@gmail.com>
2016-05-06 11:33:10 +0800

    Rewrote testing file to allow testing of basic features using 'go test'

--- lockfile_test.go
+++ lockfile_test.go
@@ -1,28 +1,64 @@
-package lockfile_test
+package lockfile
 
 import (
-	lockfile "."
 	"fmt"
-	"os"
+	"io/ioutil"
+	"math/rand"
 	"path/filepath"
+	"strconv"
+	"testing"
 )
 
-func ExampleLockfile() {
-	lock, err := lockfile.New(filepath.Join(os.TempDir(), "lock.me.now.lck"))
+func TestSimpleLock(t *testing.T) {
+	path, err := filepath.Abs("test_lockfile.pid")
 	if err != nil {
-		fmt.Printf("Cannot init lock. reason: %v\n", err)
-		panic(err)
-	}
-	err = lock.TryLock()
-
-	// Error handling is essential, as we only try to get the lock.
-	if err != nil {
-		fmt.Printf("Cannot lock \"%v\", reason: %v\n", lock, err)
 		panic(err)
 	}
 
-	defer lock.Unlock()
+	lf, err := New(path)
+	if err != nil {
+		t.Fail()
+		fmt.Println("Error making lockfile: ", err)
+		return
+	}
 
-	fmt.Println("Do stuff under lock")
-	// Output: Do stuff under lock
+	err = lf.TryLock()
+	if err != nil {
+		t.Fail()
+		fmt.Println("Error locking lockfile: ", err)
+		return
+	}
+
+	err = lf.Unlock()
+	if err != nil {
+		t.Fail()
+		fmt.Println("Error unlocking lockfile: ", err)
+		return
+	}
+}
+
+func TestDeadPID(t *testing.T) {
+	path, err := filepath.Abs("test_lockfile.pid")
+	if err != nil {
+		panic(err)
+	}
+
+	pid := GetDeadPID()
+
+	ioutil.WriteFile(path, []byte(strconv.Itoa(pid)+"\n"), 0666)
+}
+
+func GetDeadPID() int {
+	for {
+		pid := rand.Int() % 4096 //I have no idea how windows handles large PIDs, or if they even exist. Limit it to 4096 to be safe.
+		running, err := isRunning(pid)
+		if err != nil {
+			fmt.Println("Error checking PID: ", err)
+			continue
+		}
+
+		if !running {
+			return pid
+		}
+	}
 }
