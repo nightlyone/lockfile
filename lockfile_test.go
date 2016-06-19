52f65c66c1c58b138f76f9c973f9e9b6efe0b2ca
Ingo Oeser <nightlyone@googlemail.com>
2016-06-19 18:11:57 +0200

    several improvements
    and almost a small rewrite
    * improve documentation (thank you gometalinter!)
    * mark some errors as temporary
    * Unlock only files we did lock (and report rogue filesystem activity)
    * added a few helpers (and test them)

--- lockfile_test.go
+++ lockfile_test.go
@@ -13,15 +13,15 @@ import (
 func ExampleLockfile() {
 	lock, err := New(filepath.Join(os.TempDir(), "lock.me.now.lck"))
 	if err != nil {
-		fmt.Printf("Cannot init lock. reason: %v\n", err)
-		panic(err)
+		fmt.Printf("Cannot init lock. reason: %v", err)
+		panic(err) // handle properly please!
 	}
 	err = lock.TryLock()
 
 	// Error handling is essential, as we only try to get the lock.
 	if err != nil {
-		fmt.Printf("Cannot lock \"%v\", reason: %v\n", lock, err)
-		panic(err)
+		fmt.Printf("Cannot lock %q, reason: %v", lock, err)
+		panic(err) // handle properly please!
 	}
 
 	defer lock.Unlock()
@@ -30,7 +30,7 @@ func ExampleLockfile() {
 	// Output: Do stuff under lock
 }
 
-func TestLock(t *testing.T) {
+func TestBasicLockUnlock(t *testing.T) {
 	path, err := filepath.Abs("test_lockfile.pid")
 	if err != nil {
 		panic(err)
@@ -58,20 +58,20 @@ func TestLock(t *testing.T) {
 	}
 }
 
-func TestDeadPID(t *testing.T) {
-	path, err := filepath.Abs("test_lockfile.pid")
-	if err != nil {
-		panic(err)
-	}
-
-	pid := GetDeadPID()
-
-	ioutil.WriteFile(path, []byte(strconv.Itoa(pid)+"\n"), 0666)
-}
-
 func GetDeadPID() int {
-	for {
-		pid := rand.Int() % 4096 //I have no idea how windows handles large PIDs, or if they even exist. Limit it to 4096 to be safe.
+	// I have no idea how windows handles large PIDs, or if they even exist.
+	// So limit it to be less or equal to 4096 to be safe.
+
+	const maxPid = 4095
+
+	// limited iteration, so we finish one day
+	seen := map[int]bool{}
+	for len(seen) < maxPid {
+		pid := rand.Intn(maxPid + 1) // see https://godoc.org/math/rand#Intn why
+		if seen[pid] {
+			continue
+		}
+		seen[pid] = true
 		running, err := isRunning(pid)
 		if err != nil {
 			fmt.Println("Error checking PID: ", err)
@@ -82,52 +82,227 @@ func GetDeadPID() int {
 			return pid
 		}
 	}
+	panic(fmt.Sprintf("all pids lower %d are used, cannot test this", maxPid))
 }
 
 func TestBusy(t *testing.T) {
 	path, err := filepath.Abs("test_lockfile.pid")
 	if err != nil {
-		t.Fail()
-		fmt.Println(err)
+		t.Fatal(err)
 		return
 	}
 
-	lf1, err := New(path)
+	pid := os.Getppid()
+
+	if err := ioutil.WriteFile(path, []byte(strconv.Itoa(pid)+"\n"), 0666); err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer os.Remove(path)
+
+	lf, err := New(path)
 	if err != nil {
-		t.Fail()
-		fmt.Println(err)
+		t.Fatal(err)
 		return
 	}
 
-	err = lf1.TryLock()
+	got := lf.TryLock()
+	if got != ErrBusy {
+		t.Fatalf("expected error %q, got %v", ErrBusy, got)
+		return
+	}
+}
+
+func TestRogueDeletion(t *testing.T) {
+	path, err := filepath.Abs("test_lockfile.pid")
 	if err != nil {
-		t.Fail()
-		fmt.Println(err)
+		t.Fatal(err)
 		return
 	}
-
-	lf2, err := New(path)
+	lf, err := New(path)
 	if err != nil {
-		t.Fail()
-		fmt.Println(err)
+		t.Fatal(err)
 		return
 	}
-
-	err = lf2.TryLock()
-	if err == nil {
-		t.Fail()
-		fmt.Println("No error locking already locked lockfile!")
-		return
-	} else if err != ErrBusy {
-		t.Fail()
-		fmt.Println(err)
-		return
-	}
-
-	err = lf1.Unlock()
+	err = lf.TryLock()
 	if err != nil {
-		t.Fail()
-		fmt.Println(err)
+		t.Fatal(err)
 		return
+	}
+	err = os.Remove(path)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	got := lf.Unlock()
+	if got != ErrRogueDeletion {
+		t.Fatalf("unexpected error: %v", got)
+		return
+	}
+}
+
+func TestRogueDeletionDeadPid(t *testing.T) {
+	path, err := filepath.Abs("test_lockfile.pid")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	lf, err := New(path)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	err = lf.TryLock()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	pid := GetDeadPID()
+	if err := ioutil.WriteFile(path, []byte(strconv.Itoa(pid)+"\n"), 0666); err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer os.Remove(path)
+
+	err = lf.Unlock()
+	if err != ErrRogueDeletion {
+		t.Fatalf("unexpected error: %v", err)
+		return
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		t.Fatal("lockfile should not be deleted by us, if we didn't create it")
+	} else {
+		if err != nil {
+			t.Fatalf("unexpected error %v", err)
+		}
+	}
+}
+
+func TestRemovesStaleLockOnDeadOwner(t *testing.T) {
+	path, err := filepath.Abs("test_lockfile.pid")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	lf, err := New(path)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	pid := GetDeadPID()
+	if err := ioutil.WriteFile(path, []byte(strconv.Itoa(pid)+"\n"), 0666); err != nil {
+		t.Fatal(err)
+		return
+	}
+	err = lf.TryLock()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if err := lf.Unlock(); err != nil {
+		t.Fatal(err)
+		return
+	}
+}
+
+func TestInvalidPidLeadToReplacedLockfileAndSuccess(t *testing.T) {
+	path, err := filepath.Abs("test_lockfile.pid")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	if err := ioutil.WriteFile(path, []byte("\n"), 0666); err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer os.Remove(path)
+
+	lf, err := New(path)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if err := lf.TryLock(); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+		return
+	}
+
+	// now check if file exists and contains the correct content
+	got, err := ioutil.ReadFile(path)
+	if err != nil {
+		t.Fatalf("unexpected error %v", err)
+		return
+	}
+	want := fmt.Sprintf("%d\n", os.Getpid())
+	if string(got) != want {
+		t.Fatalf("got %q, want %q", got, want)
+	}
+}
+
+func TestScanPidLine(t *testing.T) {
+	tests := [...]struct {
+		input []byte
+		pid   int
+		xfail error
+	}{
+		{
+			xfail: ErrInvalidPid,
+		},
+		{
+			input: []byte(""),
+			xfail: ErrInvalidPid,
+		},
+		{
+			input: []byte("\n"),
+			xfail: ErrInvalidPid,
+		},
+		{
+			input: []byte("-1\n"),
+			xfail: ErrInvalidPid,
+		},
+		{
+			input: []byte("0\n"),
+			xfail: ErrInvalidPid,
+		},
+		{
+			input: []byte("a\n"),
+			xfail: ErrInvalidPid,
+		},
+		{
+			input: []byte("1\n"),
+			pid:   1,
+		},
+	}
+
+	// test positive cases first
+	for step, tc := range tests {
+		if tc.xfail != nil {
+			continue
+		}
+		want := tc.pid
+		got, err := scanPidLine(tc.input)
+		if err != nil {
+			t.Fatalf("%d: unexpected error %v", step, err)
+		}
+		if got != want {
+			t.Errorf("%d: expected pid %d, got %d", step, want, got)
+		}
+	}
+
+	// test negative cases now
+	for step, tc := range tests {
+		if tc.xfail == nil {
+			continue
+		}
+		want := tc.xfail
+		_, got := scanPidLine(tc.input)
+		if got != want {
+			t.Errorf("%d: expected error %v, got %v", step, want, got)
+		}
 	}
 }
